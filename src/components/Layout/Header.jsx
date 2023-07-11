83a3b233cdeee8bb86920d3fac6d14cd053b33b1
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-11 17:53:32 -0600

    Added carts and modals

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
             <img src="https://www.marthastewart.com/thmb/aVMX2qtJCGCxLa9nU18qYy9NpOw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cacio-e-pepe-opener-mld109436_horiz-1222-425e28fdf7d4403abe05b1f6ab24d106.jpg" alt="image" />
