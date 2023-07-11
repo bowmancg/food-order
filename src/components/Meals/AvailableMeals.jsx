cb6312f43c445e94f17040d89de35c8148535d44
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-11 13:47:31 -0600

    added cards and forms

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const dummy_data = [
     {
@@ -29,13 +31,15 @@ const dummy_data = [
 ]
 
 const AvailableMeals = () => {
-    const mealList = dummy_data.map(meal => <li>{meal.name}</li>)
+    const mealList = dummy_data.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
     return (
-        <section className={classes.meals}>
-            <ul>
-                {mealList}
-            </ul>
-        </section>
+        <Card>
+            <section className={classes.meals}>
+                <ul>
+                    {mealList}
+                </ul>
+            </section>
+        </Card>
     );
 }
 
